4870ad86d47821228195e6dc9dbbbc8692ec6af9
Marcus <marcus.oflaherty222@gmail.com>
2023-08-05 14:47:32 +0100

    starting files

---
+++ script.js
@@ -0,0 +1,18 @@
+const gameBoard = (function(){
+    const boardObj = {
+        boardArr: []
+    }
+    return {boardObj}
+})();
+
+const controlFlow = (function(){
+
+})()
+
+function newPlayer(name) {
+    return {
+        name: name
+    }
+}
+
+console.log(newPlayer('Marcus'))
