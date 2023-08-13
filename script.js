aeb06e381c9678fb598189f274dba939c1cce8bc
Marcus <marcus.oflaherty222@gmail.com>
2023-08-13 15:50:41 +0100

    game end logic, stlying, and basic control flow

--- script.js
+++ script.js
@@ -1,11 +1,52 @@
 const [...displayBoard] = document.querySelectorAll(".tile")
+const containerBoard = document.querySelector(".container-board")
+const modal = document.querySelector(".modal")
+const modalMsg = document.querySelector(".modal-msg")
 
 const gameBoard = (function(){
     const boardObj = {
         boardArr: ['','','',
                    '','','',
-                   '','','']
+                   '','',''],
+        gameCheck(players, turn) {
+            console.log(players[turn].team)
+            if(this.boardArr[0] == players[turn].team && this.boardArr[1] == players[turn].team && this.boardArr[2] == players[turn].team){
+                modalMsg.textContent = `${players[turn].name} is the Winner!`
+            }
+            if(this.boardArr[3] == players[turn].team && this.boardArr[4] == players[turn].team && this.boardArr[5] == players[turn].team){
+                modalMsg.textContent = `${players[turn].name} is the Winner!`
+            }
+            if(this.boardArr[6] == players[turn].team && this.boardArr[7] == players[turn].team && this.boardArr[8] == players[turn].team){
+                modalMsg.textContent = `${players[turn].name} is the Winner!`
+            }
+            if(this.boardArr[0] == players[turn].team && this.boardArr[3] == players[turn].team && this.boardArr[6] == players[turn].team){
+                modalMsg.textContent = `${players[turn].name} is the Winner!`
+            }
+            if(this.boardArr[1] == players[turn].team && this.boardArr[4] == players[turn].team && this.boardArr[7] == players[turn].team){
+                modalMsg.textContent = `${players[turn].name} is the Winner!`
+            }
+            if(this.boardArr[2] == players[turn].team && this.boardArr[5] == players[turn].team && this.boardArr[8] == players[turn].team){
+                modalMsg.textContent = `${players[turn].name} is the Winner!`
+            }
+            if(this.boardArr[0] == players[turn].team && this.boardArr[4] == players[turn].team && this.boardArr[8] == players[turn].team){
+                modalMsg.textContent = `${players[turn].name} is the Winner!`
+            }
+            if(this.boardArr[2] == players[turn].team && this.boardArr[4] == players[turn].team && this.boardArr[6] == players[turn].team){
+                modalMsg.textContent = `${players[turn].name} is the Winner!`
+            }
+        },
+        boardInit() {
+            boardObj.boardArr = 
+            ['','','',
+            '','','',
+            '','','']
+
+            displayBoard.forEach((tile) => {
+                
+            })
+        }
     }
+    
     return {boardObj}
 })();
 
@@ -14,7 +55,6 @@ const controlFlow = (function(){
     let turn = 0
     const boardUpdate = function(tile, i, player) {
         player = player[turn]
-        console.log(player)
         let index = tile.classList[1]
          if(gameBoard.boardObj.boardArr[index]) {
                 return
@@ -25,10 +65,10 @@ const controlFlow = (function(){
         displayBoard[i].classList.add('picked')
         playerPick.textContent = gameBoard.boardObj.boardArr[i]
         tile.appendChild(playerPick)
+        gameBoard.boardObj.gameCheck(players, turn)
         if(turn == 0) {
             turn = 1
         } else turn = 0
-
     }
     return {
         players,
@@ -44,8 +84,6 @@ function newPlayer(name, team) {
     }
 }
 
-// loop thru current display
-
 
 displayBoard.forEach((tile, i) => {
     tile.addEventListener('click', () => {
@@ -54,16 +92,3 @@ displayBoard.forEach((tile, i) => {
     
 })
 
-// compare current i of array with the array obj in data
-
-// let index = tile.classList[1]
-// tile.addEventListener('click', (e) => {
-//     if(gameBoard.boardObj.boardArr[index]) {
-//         return
-//     }
-//     gameBoard.boardObj.boardArr[index] = 'X'
-//     let playerPick = document.createElement('p')
-//     playerPick.classList.add('team')
-//     playerPick.textContent = gameBoard.boardObj.boardArr[i]
-//     tile.appendChild(playerPick)
-// })
