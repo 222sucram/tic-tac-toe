fbcb3396ff72a639cde760cac45421be29d582e2
Marcus <marcus.oflaherty222@gmail.com>
2023-08-17 00:57:07 +0100

    add all win and draw states

--- script.js
+++ script.js
@@ -7,26 +7,6 @@ const modalMsg = document.querySelector(".modal-msg")
 const modalBtn = document.querySelector(".game-start")
 let players = []
 
-modalBtn.addEventListener('click', () => {
-    if(!titlePlayerOne.firstChild.value || !titlePlayerTwo.firstChild.value) {
-        modalBtn.textContent = "Please enter player names."
-    } 
-
-    if(titlePlayerOne.firstChild.value && titlePlayerTwo.firstChild.value) {
-        titlePlayerOne.innerHTML = `<p>${titlePlayerOne.firstChild.value}</p>`
-        titlePlayerTwo.innerHTML = `<p>${titlePlayerTwo.firstChild.value}</p>`
-        players.push(newPlayer(titlePlayerOne.firstChild.innerHTML, 'X'))
-        players.push(newPlayer(titlePlayerTwo.firstChild.innerHTML, 'O'))
-        modalBtn.classList.toggle("hide")
-        containerBoard.classList.toggle("hide")
-        displayBoard.forEach((tile, i) => {
-            tile.addEventListener('click', () => {
-                controlFlow.boardUpdate(tile, i, players)
-            })
-            
-        })
-    }
-})
 
 const gameBoard = (function(){
     const boardObj = {
@@ -34,40 +14,77 @@ const gameBoard = (function(){
                    '','','',
                    '','',''],
         gameCheck(players, turn) {
+            let win = false
             if(this.boardArr[0] == players[turn].team && this.boardArr[1] == players[turn].team && this.boardArr[2] == players[turn].team){
-                console.log(players[turn].name)
                 modalMsg.textContent = `${players[turn].name} is the Winner!`
                 modalBtn.textContent = 'Play Again?'
                 modalBtn.classList.toggle("hide")
+                containerBoard.classList.toggle("hide")
                 modalBtn.addEventListener('click', this.boardInit)
+                win = true
             }
             if(this.boardArr[3] == players[turn].team && this.boardArr[4] == players[turn].team && this.boardArr[5] == players[turn].team){
                 modalMsg.textContent = `${players[turn].name} is the Winner!`
                 modalBtn.textContent = 'Play Again?'
+                modalBtn.classList.toggle("hide")
+                containerBoard.classList.toggle("hide")
+                modalBtn.addEventListener('click', this.boardInit)
+                win = true
             }
             if(this.boardArr[6] == players[turn].team && this.boardArr[7] == players[turn].team && this.boardArr[8] == players[turn].team){
                 modalMsg.textContent = `${players[turn].name} is the Winner!`
                 modalBtn.textContent = 'Play Again?'
+                modalBtn.classList.toggle("hide")
+                containerBoard.classList.toggle("hide")
+                modalBtn.addEventListener('click', this.boardInit)
+                win = true
             }
             if(this.boardArr[0] == players[turn].team && this.boardArr[3] == players[turn].team && this.boardArr[6] == players[turn].team){
                 modalMsg.textContent = `${players[turn].name} is the Winner!`
                 modalBtn.textContent = 'Play Again?'
+                modalBtn.classList.toggle("hide")
+                containerBoard.classList.toggle("hide")
+                modalBtn.addEventListener('click', this.boardInit)
+                win = true
             }
             if(this.boardArr[1] == players[turn].team && this.boardArr[4] == players[turn].team && this.boardArr[7] == players[turn].team){
                 modalMsg.textContent = `${players[turn].name} is the Winner!`
                 modalBtn.textContent = 'Play Again?'
+                modalBtn.classList.toggle("hide")
+                containerBoard.classList.toggle("hide")
+                modalBtn.addEventListener('click', this.boardInit)
+                win = true
             }
             if(this.boardArr[2] == players[turn].team && this.boardArr[5] == players[turn].team && this.boardArr[8] == players[turn].team){
                 modalMsg.textContent = `${players[turn].name} is the Winner!`
                 modalBtn.textContent = 'Play Again?'
+                modalBtn.classList.toggle("hide")
+                containerBoard.classList.toggle("hide")
+                modalBtn.addEventListener('click', this.boardInit)
+                win = true
             }
             if(this.boardArr[0] == players[turn].team && this.boardArr[4] == players[turn].team && this.boardArr[8] == players[turn].team){
                 modalMsg.textContent = `${players[turn].name} is the Winner!`
                 modalBtn.textContent = 'Play Again?'
+                modalBtn.classList.toggle("hide")
+                containerBoard.classList.toggle("hide")
+                modalBtn.addEventListener('click', this.boardInit)
+                win = true
             }
             if(this.boardArr[2] == players[turn].team && this.boardArr[4] == players[turn].team && this.boardArr[6] == players[turn].team){
                 modalMsg.textContent = `${players[turn].name} is the Winner!`
                 modalBtn.textContent = 'Play Again?'
+                modalBtn.classList.toggle("hide")
+                containerBoard.classList.toggle("hide")
+                modalBtn.addEventListener('click', this.boardInit)
+                win = true
+            } 
+            if(this.boardArr[0] && this.boardArr[1] && this.boardArr[2] && this.boardArr[3] && this.boardArr[4] && this.boardArr[5] && this.boardArr[6] && this.boardArr[7] && this.boardArr[8] && win == false) {
+                modalMsg.textContent = `It's a Draw!`
+                modalBtn.textContent = 'Play Again?'
+                modalBtn.classList.toggle("hide")
+                containerBoard.classList.toggle("hide")
+                modalBtn.addEventListener('click', this.boardInit)
             }
         },
         boardInit() {
@@ -76,8 +93,10 @@ const gameBoard = (function(){
             '','','',
             '','','']
 
+            containerBoard.classList.toggle("hide")
             displayBoard.forEach((tile) => {
             tile.innerHTML = ''
+            tile.classList.remove('picked')
             })
             modalBtn.classList.toggle("hide")
             modalMsg.textContent = ''
@@ -106,8 +125,33 @@ const controlFlow = (function(){
             turn = 1
         } else turn = 0
     }
+
+    const initGame = function() {
+        modalBtn.addEventListener('click', () => {
+            if(!titlePlayerOne.firstChild.value || !titlePlayerTwo.firstChild.value) {
+                modalBtn.textContent = "Please enter player names."
+            } 
+
+            if(titlePlayerOne.firstChild.value && titlePlayerTwo.firstChild.value) {
+                titlePlayerOne.innerHTML = `<p>${titlePlayerOne.firstChild.value}</p>`
+                titlePlayerTwo.innerHTML = `<p>${titlePlayerTwo.firstChild.value}</p>`
+                players.push(newPlayer(titlePlayerOne.firstChild.innerHTML, 'X'))
+                players.push(newPlayer(titlePlayerTwo.firstChild.innerHTML, 'O'))
+                modalBtn.classList.toggle("hide")
+                containerBoard.classList.toggle("hide")
+                displayBoard.forEach((tile, i) => {
+                    tile.addEventListener('click', () => {
+                        controlFlow.boardUpdate(tile, i, players)
+                    })
+                    
+                })
+            }
+        })
+    }
+    
     return {
-        boardUpdate
+        boardUpdate,
+        initGame
     }
 })()
 
@@ -119,11 +163,4 @@ function newPlayer(name, team) {
     }
 }
 
-
-// displayBoard.forEach((tile, i) => {
-//     tile.addEventListener('click', () => {
-//         controlFlow.boardUpdate(tile, i, controlFlow.players)
-//     })
-    
-// })
-
+controlFlow.initGame()
